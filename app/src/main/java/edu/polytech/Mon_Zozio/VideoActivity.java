2e909638c42815572bbec7e86c78a8fa10a474d5
Lorenzo Froment <lorenzo.froment@etu.unice.fr>
2023-03-28 17:12:53 +0200

    OPENSTREET MAP

--- app/src/main/java/edu/polytech/Mon_Zozio/VideoActivity.java
+++ app/src/main/java/edu/polytech/Mon_Zozio/VideoActivity.java
@@ -1,54 +1,70 @@
 package edu.polytech.Mon_Zozio;
-import android.content.Context;
-import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.Overlay;
+import org.osmdroid.views.overlay.OverlayItem;
+
+import java.util.ArrayList;
 
 
-
-public class VideoActivity extends AppCompatActivity implements ClickableMenuItem, ClickableActivity {
-    private Videos videos;
+public class VideoActivity extends AppCompatActivity {
+    private MapView map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Configuration.getInstance().load(getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
         setContentView(R.layout.activity_video);
+        map= findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        GeoPoint startPoint = new GeoPoint(48.8534, 2.3488);
+        IMapController  mapController = map.getController();
+        mapController.setZoom(18.0);
+        mapController.setCenter(startPoint);
+        ArrayList<OverlayItem> items = new ArrayList<>();
+        OverlayItem home= new OverlayItem("Home", "My Home", startPoint);
+        Drawable m= home.getMarker(0);
+        items.add(home);
+        items.add(new OverlayItem("Resto", "chez Luigi", new GeoPoint(48.8534, 6.3488)));
+        ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(getApplicationContext(), items, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+            @Override
+            public boolean onItemSingleTapUp(int index, OverlayItem item) {
+                return true;
+            }
 
-        FragmentMenu fragmentFame = new FragmentMenu();
-        int valeurSaisie = getIntent().getIntExtra(getString(R.string.NUM_ACTIVITY),0);
-        Bundle args = new Bundle();
-        args.putInt(getString(R.string.VALUE_FOR_MENU_FRAGMENT), valeurSaisie);
-        fragmentFame.setArguments(args);
-        getSupportFragmentManager().beginTransaction().add(R.id.fragmentMenu, (Fragment) fragmentFame) .commit();
+            @Override
+            public boolean onItemLongPress(int index, OverlayItem item) {
+                return false;
+            }
+        });
 
-        videos = new Videos();
-        VideoAdapter adapter = new VideoAdapter(this, videos);
-        ListView display = findViewById(R.id.listVideo);
-        display.setAdapter(adapter);
+        mOverlay.setFocusItemsOnTap(true);
+        map.getOverlays().add(mOverlay);
     }
-
-
 
     @Override
-    public Context getContext() {
-        return getApplicationContext();
+    public void onPause() {
+        super.onPause();
+        map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
     }
-
     @Override
-    public void onClick(int position) {
-        Culture title = new Culture( videos.get(position).getName(), videos.get(position).getDuration(), videos.get(position).getPicture() );
-        //Toast.makeText(getApplicationContext(), "item Clicked = "+ title, Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(getApplicationContext(), DisplayActivity.class);
-        intent.putExtra(getString(R.string.TITLE), title);
-        startActivity(intent);
+    public void onResume() {
+        super.onResume();
+        map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
     }
 
-
-    @Override
-    public String getKeyValue(int id) {
-        return getString(R.string.NUM_ACTIVITY);
-    }
 }
