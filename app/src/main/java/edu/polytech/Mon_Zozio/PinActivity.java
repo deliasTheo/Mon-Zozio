aad3440b3d2479c6665c139d6b62f49c619fbb91
Lorenzo Froment <lorenzo.froment@etu.unice.fr>
2023-04-25 16:48:59 +0200

    j'ai ajouté le fragment menu à la map

--- app/src/main/java/edu/polytech/Mon_Zozio/PinActivity.java
+++ app/src/main/java/edu/polytech/Mon_Zozio/PinActivity.java
@@ -1,11 +1,12 @@
 package edu.polytech.Mon_Zozio;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Window;
-import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceManager;
 
 import org.osmdroid.api.IMapController;
@@ -14,25 +15,23 @@ import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
 import org.osmdroid.config.Configuration;
-import org.osmdroid.views.overlay.ItemizedIconOverlay;
-import org.osmdroid.views.overlay.ItemizedOverlay;
 import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
-import org.osmdroid.views.overlay.Overlay;
 import org.osmdroid.views.overlay.OverlayItem;
 
 import java.util.ArrayList;
 
 
-public class VideoActivity extends AppCompatActivity {
+public class PinActivity extends AppCompatActivity implements ClickableMenuItem<Integer>,ClickableActivity{
     private MapView map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
+        FragmentMenu fragmentFame = new FragmentMenu();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragmentMenu, (Fragment) fragmentFame) .commit();
         Configuration.getInstance().load(getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
-        setContentView(R.layout.activity_video);
+        setContentView(R.layout.pin_activity);
         map = findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
         map.setBuiltInZoomControls(true);
@@ -82,4 +81,18 @@ public class VideoActivity extends AppCompatActivity {
         map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
     }
 
+    @Override
+    public Context getContext() {
+        return getApplicationContext();
+    }
+
+    @Override
+    public String getKeyValue(int id) {
+        return getString(R.string.NUM_ACTIVITY);
+    }
+
+    @Override
+    public void onClick(int position) {
+
+    }
 }
