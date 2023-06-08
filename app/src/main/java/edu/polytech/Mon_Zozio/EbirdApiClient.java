af016aca4ada601222ba58b38aff782c2ffe49c7
komi-assimpah <komi-jean-paul.assimpah@etu.unice.fr>
2023-06-08 17:13:43 +0200

    error 403 with okhttp3 and volley 2

--- app/src/main/java/edu/polytech/Mon_Zozio/EbirdApiClient.java
+++ app/src/main/java/edu/polytech/Mon_Zozio/EbirdApiClient.java
@@ -6,13 +6,10 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import okhttp3.*;
-<<<<<<< HEAD
+//import okhttp3.*;
 import com.android.volley.Request;
-=======
->>>>>>> a11059320b8928f08831a53a26589e5f1246d3cc
 import com.android.volley.RequestQueue;
-//import com.android.volley.Response;
+import com.android.volley.Response;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
@@ -31,48 +28,40 @@ public class EbirdApiClient {
         requestQueue = Volley.newRequestQueue(context);
     }
 
-    public void getRecentObservations() {
-        OkHttpClient client = new OkHttpClient().newBuilder()
-                .build();
-        MediaType mediaType = MediaType.parse("text/plain");
-        RequestBody body = RequestBody.create(mediaType, "");
-        Request request = new Request.Builder()
-                .url("https://api.ebird.org/v2/data/obs/{{regionCode}}/recent/notable?detail=full")
-                .method("GET", body)
-                .addHeader("X-eBirdApiToken", "{{x-ebirdapitoken}}")
-                .build();
+    public void getRecentObservations(Response.Listener<JSONObject> successListener, Response.ErrorListener errorListener) {
+        String url = BASE_URL + "data/obs/" + regionCode + "/geo/recent";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, successListener, errorListener) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("X-eBirdApiToken", API_KEY);
+                return headers;
+            }
+        };
+        requestQueue.add(request);
 
-        try (Response response = client.newCall(request).execute()) {
-            String responseBody = response.body().string();
-            // ... do something with response
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+
+
+        /*public void getRecentObservations() {
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .build();
+            MediaType mediaType = MediaType.parse("text/plain");
+            RequestBody body = RequestBody.create(mediaType, "");
+            Request request = new Request.Builder()
+                    .url("https://api.ebird.org/v2/data/obs/{{regionCode}}/recent/notable?detail=full")
+                    .method("GET", body)
+                    .addHeader("X-eBirdApiToken", "{{x-ebirdapitoken}}")
+                    .build();
+
+            try (Response response = client.newCall(request).execute()) {
+                String responseBody = response.body().string();
+                // ... do something with response
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }*/
+
     }
-
-
-
-
-    /*public void getRecentObservations() {
-        OkHttpClient client = new OkHttpClient().newBuilder()
-                .build();
-        MediaType mediaType = MediaType.parse("text/plain");
-        RequestBody body = RequestBody.create(mediaType, "");
-        Request request = new Request.Builder()
-                .url("https://api.ebird.org/v2/data/obs/{{regionCode}}/recent/notable?detail=full")
-                .method("GET", body)
-                //.get()
-                .addHeader("X-eBirdApiToken", "{{x-ebirdapitoken}}")
-                .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            String responseBody = response.body().string();
-            // ... do something with response
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }*/
-
 
 
 
--- app/src/main/java/edu/polytech/Mon_Zozio/MainActivity.java
+++ app/src/main/java/edu/polytech/Mon_Zozio/MainActivity.java
@@ -81,7 +81,6 @@ public class MainActivity extends AppCompatActivity implements ClickableMenuItem
 
 
         // Make the API request to get recent observations
-<<<<<<< HEAD
         eBirdApiClient.getRecentObservations(new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
@@ -105,10 +104,6 @@ public class MainActivity extends AppCompatActivity implements ClickableMenuItem
         });
 
 
-
-=======
-        eBirdApiClient.getRecentObservations();
->>>>>>> a11059320b8928f08831a53a26589e5f1246d3cc
     }
 
     @Override
